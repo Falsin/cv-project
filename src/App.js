5e46a70480fb582a07dbb5092405b751fa237798
Falsin <greenldend@gmail.com>
2021-05-08 07:59:28 +0700

    changed the App.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import constructor from './images/сonstructor.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> vmfkkbkbnvn
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
     </div>
   );
+}
+
+class Header extends React.Component {
+  render() {
+    return(
+      <header>
+        <div className='container'>
+          <img src={constructor} alt=''></img>
+          <h1>CV Сonstructor</h1>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default App;
