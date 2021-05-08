730d5afd7bd21cee97e700479845b648a021dfef
Falsin <greenldend@gmail.com>
2021-05-08 11:19:11 +0700

    setting the templateCV.js component

--- src/components/templateCV.js
+++ src/components/templateCV.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
+let countryList;
+
+function getCountryList() {
+  return fetch(`https://restcountries.eu/rest/v2/all`, {mode: 'cors'})
+  .then(response => response.json())
+  .then(response => countryList = response)
+}
+
+getCountryList()
+
+
 class TemplateCV extends React.Component {
+
+  changeHandler(e) {
+    console.log(e.target.value)
+    console.log(countryList)
+
+  }
+
   render() {
     return (
       <section>
         <div id='generalInfo'>
           <div>
-            <label>Name<input></input></label>
-            <label>Email<input></input></label>
-            <label>Phone<input></input></label>
-            <label>Country<input></input></label>
+            <label htmlFor='name'>Name</label>
+            <input id='name'></input>
+
+            <label htmlFor='email'>Email</label>
+            <input id='email' type='email'></input>
+
+            <label htmlFor='phone' type='tel'>Phone</label>
+            <input id='phone'></input>
+
+            <label htmlFor='country'>Country</label>
+            <input id='country' type='country' onChange={(e) => this.changeHandler(e)}></input>
           </div>
           <div className='photo'>Hello!</div>
         </div>
