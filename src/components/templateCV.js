68192418c5b9702ee713e1911fe5d0b05c22255b
Falsin <greenldend@gmail.com>
2021-05-08 08:32:05 +0700

    changes styles

--- src/components/templateCV.js
+++ src/components/templateCV.js
@@ -7,10 +7,9 @@ class TemplateCV extends React.Component {
         <div id='generalInfo'>
           <div>
             <label>Name<input></input></label>
-            <label>E-mail<input></input></label>
+            <label>Email<input></input></label>
             <label>Phone<input></input></label>
             <label>Country<input></input></label>
-            
           </div>
           <div className='photo'>Hello!</div>
         </div>
