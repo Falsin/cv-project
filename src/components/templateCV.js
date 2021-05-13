db99a4a2a2670aab008388594c24dd68d14826f0
Falsin <greenldend@gmail.com>
2021-05-13 18:32:25 +0700

    fixed the templateCV component

--- src/components/templateCV.js
+++ src/components/templateCV.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import uniqid from 'uniqid';
 
 class TemplateCV extends React.Component {
   constructor(props) {
@@ -6,8 +7,163 @@ class TemplateCV extends React.Component {
 
     this.state = {
       activeElemArr: [],
-      cityNamesArr: [],
-      enteredVal: '',
+      countryNamesArr: [],
+      enteredVal: {
+        value: '',
+        id: uniqid()
+      },
+    }
+  }
+
+/*   changeHandler(e) {
+    console.log(e.target.value.length === 0)
+    if (e.target.value.length > 0) {
+      if (![...e.target.classList].includes('active')) {
+        e.target.classList.add('active')
+        this.setState({
+          activeElemArr: [...this.state.activeElemArr, e.target]
+        })
+      }
+      if (e.target.id === 'country') {
+        this.setState({
+          enteredVal: {
+            value: e.target.value,
+            id: uniqid(),
+          }
+        })
+      }
+      
+    } else if (!e.target.value.length) {
+      console.log('remove')
+      e.target.classList.remove('active')
+      const index = this.state.activeElemArr.indexOf(e.target);
+      const newArr = this.state.activeElemArr;
+      newArr.splice(index, 1);
+      this.setState({
+        activeElemArr: newArr,
+      })
+    }
+  } */
+
+  changeHandler(e) {
+    if (e.target.value.length > 0 && ![...e.target.classList].includes('active')) {
+      e.target.classList.add('active')
+      this.setState({
+        activeElemArr: [...this.state.activeElemArr, e.target]
+      })
+    } else if (!e.target.value.length) {
+      e.target.classList.remove('active');
+      this.setState({
+        activeElemArr: this.state.activeElemArr.filter(elem => elem !== e.target),
+      })
+    }
+  }
+
+  returnParentScope() {
+    return this;
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <section>
+        <div id='generalInfo'>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input id='name' onChange={this.changeHandler.bind(this)}></input>
+
+            <label htmlFor='email'>Email</label>
+            <input id='email' type='email' onChange={this.changeHandler.bind(this)}></input>
+
+            <label htmlFor='phone' type='tel'>Phone</label>
+            <input id='phone' onChange={this.changeHandler.bind(this)}></input>
+
+            <CountryComp parentScope={this.returnParentScope.bind(this)}/>
+          </div>
+
+          <div className='photo'>
+            <input type='file'></input>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+class CountryComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.parentScope = props.parentScope();
+  }
+
+  getCountryList() {
+    return fetch(`https://restcountries.eu/rest/v2/all`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(response => {
+      this.parentScope.setState({
+        countryNamesArr: response,
+      })
+    })
+  }
+
+  componentDidMount(prevProps, prevState) {
+    if (prevState !== this.state) {
+      this.getCountryList()
+    }
+  }
+
+  createListElements() {
+    let collection = [];
+    let enteredVal = this.parentScope.state.enteredVal.value;
+
+    for (const {name} of this.parentScope.state.countryNamesArr) {
+      if(enteredVal.length >= 2 && name.includes(enteredVal)) {
+        collection.push({
+          id: uniqid(),
+          name: name,
+        });
+      }
+    }
+    return collection;
+  }
+
+  enteredValHandler(e) {
+    this.parentScope.changeHandler.bind(this.parentScope);
+    this.parentScope.setState({
+      enteredVal: {
+        value: e.target.value,
+        id: uniqid(),
+      }
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <label htmlFor='country'>Country</label>
+            {/* <input id='country' type='text' onChange={this.parentScope.changeHandler.bind(this.parentScope)} list='cityName'></input> */}
+            <input id='country' type='text' onChange={this.enteredValHandler.bind(this)} list='cityName'></input>
+            <datalist id='cityName'>
+               {this.createListElements().map(elem => {
+                return <option key={elem.id}>{elem.name}</option>
+              })} 
+            </datalist>
+      </div>
+    )
+  }
+}
+
+/* class TemplateCV extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeElemArr: [],
+      countryNamesArr: [],
+      enteredVal: {
+        value: '',
+        id: uniqid()
+      },
     }
   }
 
@@ -16,7 +172,7 @@ class TemplateCV extends React.Component {
     .then(response => response.json())
     .then(response => {
       this.setState({
-        cityNamesArr: response,
+        countryNamesArr: response,
       })
     })
   }
@@ -37,12 +193,34 @@ class TemplateCV extends React.Component {
       }
       if (e.target.id === 'country') {
         this.setState({
-          enteredVal: e.target.value
+          enteredVal: {
+            value: e.target.value,
+            id: uniqid(),
+          }
         })
       }
     } else if (!e.target.value.length) {
       e.target.classList.remove('active')
     }
+  }
+
+  createListElements() {
+    let collection = [];
+    let enteredVal = this.state.enteredVal.value;
+
+    for (const {name} of this.state.countryNamesArr) {
+      if(enteredVal.length >= 2 && name.includes(enteredVal)) {
+        collection.push({
+          id: uniqid(),
+          name: name,
+        });
+      }
+    }
+    return collection;
+  }
+
+  returnParentScope() {
+    return this;
   }
 
   render() {
@@ -58,16 +236,12 @@ class TemplateCV extends React.Component {
 
             <label htmlFor='phone' type='tel'>Phone</label>
             <input id='phone' onChange={this.changeHandler.bind(this)}></input>
-
             <label htmlFor='country'>Country</label>
             <input id='country' type='text' onChange={this.changeHandler.bind(this)} list='cityName'></input>
             <datalist id='cityName'>
-              {this.state.cityNamesArr.map(item => {
-                if (this.state.enteredVal.length >= 2 && item.name.includes(this.state.enteredVal)) {
-                  return <option>{item.name}</option>
-                }
-                return <option></option>
-              })}
+               {this.createListElements().map(elem => {
+                return <option key={elem.id}>{elem.name}</option>
+              })} 
             </datalist>
           </div>
 
@@ -78,6 +252,8 @@ class TemplateCV extends React.Component {
       </section>
     )
   }
-}
+} */
+
+
 
 export default TemplateCV;
