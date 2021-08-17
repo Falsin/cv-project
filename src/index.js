48f62e1d1105e1b8de6565405bf19f3dc5b7813a
Falsin <greenldend@gmail.com>
2021-08-17 20:17:07 +0700

    d

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <CommonParentComponent />
 {/*         <CommonParentComponent />
         <Footer /> */}
       </div>
